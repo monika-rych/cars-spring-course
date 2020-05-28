46ff51af40b0c77db8494d8411092502febb816e
Monika <monika.k.rychlicka@gmail.com>
2020-05-29 00:46:40 +0100

    in momory not needed really

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,8 +8,6 @@ import { FormsModule } from '@angular/forms';
 import { CarDetailComponent } from './car-detail/car-detail.component';
 import { MessageComponent } from './message/message.component';
 import { HttpClientModule }    from '@angular/common/http';
-import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { InMemoryDataService }  from './in-memory-data.service';
 
 @NgModule({
   declarations: [
@@ -22,14 +20,7 @@ import { InMemoryDataService }  from './in-memory-data.service';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule,
-
-    // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
-    // and returns simulated server responses.
-    // Remove it when a real server is ready to receive requests.
-    HttpClientInMemoryWebApiModule.forRoot(
-      InMemoryDataService, { dataEncapsulation: false }
-    )
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
