86fccf8b5548a3103c4c28796fe30c3b174d25e2
Monika <monika.k.rychlicka@gmail.com>
2020-05-30 22:03:16 +0100

    new component add Car

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CarsComponent } from './cars/cars.component';
 import { CarDetailComponent } from './car-detail/car-detail.component'
+import { AddCarComponent } from './add-car/add-car.component';
 
 const routes: Routes = [
   { path: 'detail/:id', component: CarDetailComponent },
-  {path: 'cars', component: CarsComponent }
+  {path: 'cars', component: CarsComponent },
+  {path: 'add-car', component: AddCarComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,15 @@ import { FormsModule } from '@angular/forms';
 import { CarDetailComponent } from './car-detail/car-detail.component';
 import { MessageComponent } from './message/message.component';
 import { HttpClientModule }    from '@angular/common/http';
+import { AddCarComponent } from './add-car/add-car.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CarsComponent,
     CarDetailComponent,
-    MessageComponent
+    MessageComponent,
+    AddCarComponent
   ],
   imports: [
     BrowserModule,
